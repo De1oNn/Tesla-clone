1441fabb3b6ac706a94ab1d81a38829d3cb2a9cf
De1oNn <nasanjargal20040524@gmail.com>
2024-11-25 14:07:00 +0800

    first commit

---
+++ pic/We-Are-Tesla_files/geo4.js
@@ -0,0 +1,17 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "ULAN BATOR",
+      'continent': "AS",
+      'country': "MN",
+      'region': "1",
+      'dma': ""
+    },
+    'ip':"202.21.127.85"
+  }]);
+})
+
+()
+
+;
